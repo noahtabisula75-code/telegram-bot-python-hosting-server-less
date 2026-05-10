b01ccb2215921a7c9c9cea9b408c4c80861108d5
CodeHax - ItsMeJeff <noahtabisula75@gmail.com>
2026-05-10 20:49:33 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BotDashboard } from "./components/BotDashboard";
+
+export default function App() {
+  return (
+    <div className="antialiased">
+      <BotDashboard />
+    </div>
+  );
+}
